443f4c827ac2446375c62aedfc3aafaf5b2a3ea1
Simosnsj <simon.jardnas.te16d@gmail.com>
2018-02-26 10:28:51 +0100

    göt så att jag bara kan hämta och sänka svårighetsgrad från ett ställe

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,21 +1,46 @@
 
 public class Hangman {
 
-	private int Liv;
-	public void difficulty(int i) {
+	private int liv;
+	
+	public void setDifficulty(int i) {
+		
+		if(liv == null) {
+			Difficulty(i);
+		}
+		else
+		
+		
+	}
+	
+	private void Difficulty(int i) {
 
 		if(i == 1) {
 			System.out.println("Du har valt lätt svårighetsgrad");
+			
+			liv = 12;
 		}
 		else if(i==2) {
 			System.out.println("Du har valt medel svårighetsgrad");
+			liv = 10;
 		}
 		else if(i ==3) {
 			System.out.println("Du har valt svår svårighetsgrad");
+			liv = 7;
 		}
 		else {
 			System.out.println("index out of bounds");
+		
 		}	
 	}
+	public int getLiv(){
+		return liv;
+		
+	}
+	public void loseLiv() {
+		liv--;
+		
+	}
+	
 }
 
