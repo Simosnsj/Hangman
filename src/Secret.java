76a27a1411a5c4147c32c401ce2deade4c7f6781
Simosnsj <simon.jardnas.te16d@gmail.com>
2018-02-28 20:11:29 +0100

    skapat sätt att läsa av filen på riktigt

--- src/Secret.java
+++ src/Secret.java
@@ -1,40 +1,52 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Secret {
-
+	private ArrayList<String> words = new ArrayList<String>();
 	private String word;
 	private String secret;
-	public Secret(int Liv) {
+	public Secret() throws FileNotFoundException {
 		newSecret();
 		sequence();
 	}
-	public void newSecret() {
-		string word = new File("ord.txt");
-		this.word = word;
-		this.secret="";
+	public void newSecret() throws FileNotFoundException {
+		Scanner sc = new Scanner(new File("ord.text"));
+
+		for(int i = 0; sc.hasNextLine(); i++) {
+			words.add(sc.nextLine());
+
+		}
 		
+		
+		
+		String word = new String();
+		this.word = word;
+		this.secret= new String();
+
 		for(int i = 0 ; i < word.length() ; i++) {
 			secret +="_";
 
 		}
 	}
-	
+
 	public void sequence() {
-		
-		
-	
+
+
+
 	}
-	
-	
-	
+
+
+
 	public boolean guess(char c) {
-			
+
 		if(word.contains(""+c)) {
-			
-			
+
+
 			return true;
 		}
-		
+
 		else{
 			return false;
 		}
